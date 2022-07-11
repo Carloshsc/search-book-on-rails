5239197e521a8a3a623ba7edc0a57daae9d68137
carlosH <carloshsc1998@gmail.com>
2022-07-11 00:22:39 -0300

    book search and registration

--- app/javascript/controllers/books_controller.js
+++ app/javascript/controllers/books_controller.js
@@ -1,34 +1,48 @@
 import { Controller } from "@hotwired/stimulus"
 
+
 export default class extends Controller {
   static targets = ["output"]
 
+  
+  searchBooks(event){
 
+    let books;
 
-  searchBooks(element){
-    let booksNames =[];
-    let url = `http://openlibrary.org/search.json?q=${element}&fields=author_name,first_publish_year,title,cover_edition_key&limit=5`;
-    fetch(url)
-    .then((response) => response.json())
-    .then((data)=>{
-      booksNames = data.docs.map((x) => x.title)
-      console.log("lista   " + booksNames);
-      console.log(data);
-    })
+    let element = event.target.value;
 
-  }
+        $('<button/>').addClass('dropdown-item').attr('type', 'button').appendTo('#find-book');
+            
+        let url = `http://openlibrary.org/search.json?q=${element}&fields=author_name,first_publish_year,title,cover_edition_key&limit=5`;
+        fetch(url)
+        .then((response) => response.json())
+        .then((data)=>{
+          books = data.docs;
 
-  updateFilter(event){
-
-    
-    this.searchBooks(event.target.value);
-
-  }
-
-
-  connect() {
-    //this.outputTarget.textContent = "Hello World!"
-    
-    
-  }
+          let list = $('#find-book')
+          list.empty()
+          $.each(books, (i) => {
+              const li = $('<li/>').appendTo(list);
+              const text = $('<p class="mb-0"/>').text(`${books[i].title}`)
+              const div = $('<div/>').append(text)
+              $(`<p class="color-grey mb-0">${books[i].author_name}</>`).appendTo(div)
+              const button = $('<button class="d-flex gap-2"/>')
+              button.addClass('dropdown-item').attr('type', 'button')
+                  .on('click', () => passBook(books[i])).appendTo(li)
+                  .append(div);               
+              
+            });
+        })
+    }
 }
+
+function passBook(book) {
+  $('#book_title').val(book.title)
+  $('#book_author').val(book.author_name)
+  $('#book_publication_year').val(book.first_publish_year)
+  if(book.cover_edition_key == null){
+    $('#book_cover_id').val('default_book.png')
+  }else $('#book_cover_id').val(`https://covers.openlibrary.org/b/olid/${book.cover_edition_key}-M.jpg`)
+  
+}
+
